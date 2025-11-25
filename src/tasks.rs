cdffbddeb7b130ca733df43091046634780a24de
Anhar Miah <anharhussainmiah@googlemail.com>
2025-11-25 22:39:50 +0000

    fix task not streaming

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,5 +1,5 @@
 use crate::Grind;
-use crate::util::shell;
+use crate::util::shell_stream;
 
 pub fn execute_task(grind: Grind, task: String) {
     if task.to_lowercase() == "list" {
@@ -19,8 +19,7 @@ pub fn execute_task(grind: Grind, task: String) {
 
     if let Some((k, v)) = target {
         println!("==> executing task [{}]", k);
-        let out = shell(v);
-        println!("{}", out);
+        let _ = shell_stream(v);
     } else {
         println!("unknown task '{}', available tasks:\n", task);
         for k in grind.project.tasks.keys() {
